51336c50ba93c76479e142b13de76d5f937a0cc3
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-09-28 11:47:47 +0200

    dev: added client node

--- client.js
+++ client.js
@@ -1,1 +1,14 @@
+import { createClient } from "redis";
+
 console.log("about to connect to redis");
+const client = await createClient({
+  url: "redis://localhost:6379",
+})
+  .on("error", (err) => console.log("Redis Client Error", err))
+  .connect();
+
+console.log("Connected with success to redis");
+
+await client.set("key", "value");
+const value = await client.get("key");
+await client.disconnect();
